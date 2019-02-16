591e8f2cfef1537f97de4332eccb4e32ad15ed31
Anderson Du <d913084825@outlook.com>
2019-02-16 01:01:34 -0800

    final project

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const path = require('path');
-
 const handlebars = require('express-handlebars');
 
 app.engine('.hbs', handlebars({ extname: '.hbs' }));
--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,8 @@ CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+DROP DATABASE IF EXISTS resume_db;
+CREATE DATABASE resume_db;
+
+
